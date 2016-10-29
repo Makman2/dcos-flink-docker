56d70ffdbca829ee8cd300011cc9be5d3ee83b18
Makman2 <makman@alice.de>
2016-10-29 22:31:10 +0200

    localpackaging

---
+++ pack_flink.sh
@@ -0,0 +1,9 @@
+set -e
+
+mkdir -p flink/flink-1.2-SNAPSHOT
+cp -v -a ~/dev/makman2-flink/build-target/. ~/dev/makman2-flink-mesos-demo/flink/flink-1.2-SNAPSHOT/.
+cd ~/dev/dcos-flink-docker/flink
+tar -cvzf flink-1.2-DEV-SNAPSHOT-bin-hadoop2.tar.gz flink-1.2-SNAPSHOT
+cd ..
+docker build -t makman2/dcos-flink .
+docker push makman2/dcos-flink
